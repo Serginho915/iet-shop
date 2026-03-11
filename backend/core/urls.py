069bc4a92bac5974bbeacf56a3025631554eadfa
haldaniko <haldaniko@gmail.com>
2026-03-11 16:44:21 +0100

    update media

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.views.static import serve
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/', include('api.urls')),
+
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    re_path(
+        r'^api/media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
